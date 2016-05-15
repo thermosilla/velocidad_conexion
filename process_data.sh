5529d94a42f9e13af0701ec48b13bad17513118d
Tomás Hermosilla <thermosilla@gmail.com>
2016-05-15 15:55:56 -0400

    Uso de biblioteca tsfactors
    - Se usa biblioteca tsfactors
    - Se agrega subida automática a blog

--- process_data.sh
+++ process_data.sh
@@ -4,3 +4,4 @@ rsync -aA nuc:test-velocidad.txt .
 awk '{gsub ("\\(","",$3); gsub(",",".",$3); gsub("\\)","",$4); print $1, $2 ",", $3 ",", $4}' test-velocidad.txt > test-velocidad.txt
 R -e 'library(rmarkdown);rmarkdown::render("velocidad_navegacion.rmd")'
 xdg-open velocidad_navegacion.html
+aws s3 cp velocidad_navegacion.html s3://blog.thermosilla.info
