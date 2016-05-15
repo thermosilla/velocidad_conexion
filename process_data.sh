9c23c75b1f5f3f16274951c7f69d28f7c2604134
Tomás Hermosilla <thermosilla@gmail.com>
2016-05-14 22:23:11 -0300

    Mejora en script de procesamiento

--- process_data.sh
+++ process_data.sh
@@ -2,5 +2,5 @@
 rm test-velocidad.txt
 rsync -aA nuc:test-velocidad.txt .
 awk '{gsub ("\\(","",$3); gsub(",",".",$3); gsub("\\)","",$4); print $1, $2 ",", $3 ",", $4}' test-velocidad.txt > test-velocidad.txt
-Rscript velocidad_navegacion.r
+R -e 'library(rmarkdown);rmarkdown::render("velocidad_navegacion.rmd")'
 xdg-open velocidad_navegacion.html
--- velocidad_navegacion.r
+++
@@ -1,2 +0,0 @@
-library("rmarkdown")
-rmarkdown::render("velocidad_navegacion.rmd")
